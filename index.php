b0d2a8821a1dbdfc8fa93ce631e8625eee4e625b
gabrielebottari <gabriele.bottari1@gmail.com>
2024-02-06 13:58:56 +0100

    ADD bootstrapp Tables

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 
     ];
 
-    // Stampa i dati degli hotel
+    /* Stampa i dati degli hotel
     echo '<ul>';
     foreach ($hotels as $hotel) {
         echo '<li>';
@@ -52,6 +52,61 @@
         echo '</li>';
     }
     echo '</ul>';
+    */
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="bg-success-subtle text-success">
+
+    <header>
+        <div class="container p-5">
+            <h1 class="text-center">PHP Hotel</h1>
+        </div>
+    </header>
+
+
+    <main>
+        <div class="container">
+        <table class="table table-info table-bordered border-success table-striped-columns table-hover text-success">
+            <thead>
+                <tr>
+                    <th class="text-success">Nome</th>
+                    <th class="text-success">Descrizione</th>
+                    <th class="text-success">Parcheggio</th>
+                    <th class="text-success">Voto</th>
+                    <th class="text-success">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($hotels as $hotel) {
+                    echo '<tr>';
+                    echo '<td>' . $hotel['name'] . '</td>';
+                    echo '<td>' . $hotel['description'] . '</td>';
+                    echo '<td>' . ($hotel['parking'] ? 'Si' : 'No') . '</td>';
+                    echo '<td>' . $hotel['vote'] . ' stelle</td>';
+                    echo '<td>' . $hotel['distance_to_center'] . ' km</td>';
+                    echo '</tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+         </div>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
